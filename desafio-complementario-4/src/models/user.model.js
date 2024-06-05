b7efafe6474392120f6d9e07537eed7cc9b7279a
Brenda Guardines <bguardines@gmail.com>
2024-06-04 22:40:09 -0300

    Subo cuarto desafio complementario

---
+++ desafio-complementario-4/src/models/user.model.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    first_name: {
+        type: String, 
+        required: true
+    },
+
+    last_name: {
+        type: String, 
+        required: true
+    },
+
+    email: {
+        type: String, 
+        required: true,
+        index: true, 
+        unique: true
+    }, 
+
+    age: {
+        type: Number,
+        required: true
+    },
+
+    password: {
+        type: String, 
+        required: true
+    },
+
+    role: {
+        type: String,
+        enum:['admin', 'usuario', 'premium'],
+        default: 'usuario'
+        // required: true
+    },
+    
+    cart: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Cart'
+    },
+
+    resetToken: {
+        token: String,
+        expire: Date
+    }
+});
+
+const UserModel = mongoose.model("user", userSchema);
+
+module.exports = UserModel;
